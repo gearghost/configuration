0acd30af44bfdc334b5f30b9245bf82c668a9699
mono <gearghost@mini.local>
2022-06-21 09:50:02 +0800

    add init.lua that fully transferring from init.vim and lua lsp support

---
+++ nvim/init.lua
@@ -0,0 +1,99 @@
+-- basics
+vim.opt.syntax = 'on'
+vim.opt.encoding = 'utf-8'
+vim.opt.pastetoggle = '<F11>'
+vim.opt.errorbells = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = true
+vim.opt.showcmd = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.showmatch = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ruler = true
+vim.opt.ttimeout = true -- time out for key codes
+vim.opt.ttimeoutlen = 10 -- wait up to 10ms after Esc for special key
+vim.opt.termguicolors = false -- disable true code support within neovim
+
+-- highlights
+vim.api.nvim_exec(
+[[
+  highlight Comment ctermfg=green guifg=green
+  highlight LineNr ctermfg=grey guifg=grey
+  highlight Pmenu ctermfg=black ctermbg=white guifg=black guibg=white
+  highlight Pmenusel ctermfg=green ctermbg=blue guifg=green guibg=blue
+]],false)
+
+-- highlight Jenkinsfile with groovy syntax
+-- vim-commentary for assembly file
+-- disable automatic comment insertion
+vim.api.nvim_exec(
+[[
+  autocmd BufNewFile,BufRead Jenkinsfile setf groovy
+  autocmd FileType S,s,asm setlocal commentstring=#\ %s
+  autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+]],false)
+
+-- vim-plug@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin','~/.config/nvim/plugged')
+
+Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install()']})
+Plug('junegunn/fzf.vim')
+
+-- lsp and code completion
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+
+-- using vsnip engine
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
+
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn['TSUpdate']})
+
+Plug('tpope/vim-fugitive')
+Plug('tpope/vim-surround')
+Plug('tpope/vim-repeat')
+Plug('tpope/vim-commentary')
+
+vim.call('plug#end')
+-- @vim-plug
+
+-- key shortcut
+vim.g.mapleader= ','
+
+vim.keymap.set('n','<Leader>b',':Buffers<CR>')
+vim.keymap.set('n','<Leader>f',':Rg<CR>')
+vim.keymap.set('n','<Leader>c',':Commits<CR>')
+vim.keymap.set('n','<Leader>h',':History<CR>')
+vim.keymap.set('n','<Leader>t',':Tags<CR>')
+vim.keymap.set('n','<Leader>?',':Helptags<CR>')
+
+vim.keymap.set('n','<C-p>',':Files<CR>')
+
+vim.keymap.set('v','J',':m \'>+1<CR>gv=gv')
+vim.keymap.set('v','K',':m \'<-2<CR>gv=gv')
+
+-- fzf configuration
+vim.env.FZF_DEFAULT_COMMAND = 'rg --files'
+vim.cmd('command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {\'options\': \'--delimiter : --nth 4..\'}, <bang>0)')
+
+-- netrw configuration
+vim.g['netrw_liststyle'] = 3
+vim.g['netrw_list_hide'] = '.*\\.swp$,.DS_Store,*/tmp/*,*.swp,*.zip,^\\.git/$'
+
+-- import lsp configuration
+require 'lsp_config'
--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -99,14 +99,47 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'jedi_language_server', 'rust_analyzer', 'tsserver', 'clangd', 'gopls', 'bashls' }
+local servers = { 'sumneko_lua', 'jedi_language_server', 'rust_analyzer', 'tsserver', 'clangd', 'gopls', 'bashls' }
 for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
+  -- for lua lsp server
+	if (lsp == 'sumneko_lua')
+	then
+    require('lspconfig')[lsp].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      flags = {
+        -- This will be the default in neovim 0.7+
+        debounce_text_changes = 150,
+      },
+      settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
   }
+	else
+    require('lspconfig')[lsp].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      flags = {
+        -- This will be the default in neovim 0.7+
+        debounce_text_changes = 150,
+      }
+    }
+	end
 end
